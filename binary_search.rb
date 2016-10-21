9e14d52081b383d13927f49b9bf15da4047da948
agdavid <antonio.g.david@gmail.com>
2016-10-21 13:52:55 -0400

    start binary search; add range bookends

---
+++ binary_search.rb
@@ -0,0 +1,8 @@
+# given a number and a collection, find the number with least time complexity
+def find_target(target, array)
+
+  # create the start and finish of the search range
+  floor_index = -1
+  ceiling_index = array.length
+
+end
