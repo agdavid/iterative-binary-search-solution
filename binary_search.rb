f750eae36f7c892cdcc154b19a8ef603e359fb0b
agdavid <antonio.g.david@gmail.com>
2016-10-21 13:54:46 -0400

    begin iterative while loop with conditional

--- binary_search.rb
+++ binary_search.rb
@@ -1,8 +1,13 @@
 # given a number and a collection, find the number with least time complexity
 def find_target(target, array)
 
-  # create the start and finish of the search range
+  # create "bookends" - the start and finish of the search range
   floor_index = -1
   ceiling_index = array.length
+
+  # always need to make sure at least one value between bookends
+  while floor_index+1 < array.length
+
+  end
 
 end
