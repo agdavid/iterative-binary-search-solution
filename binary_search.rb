3dd6638ad61f1b82ddad9fafcac7fc8b17ece7a7
agdavid <antonio.g.david@gmail.com>
2016-10-21 13:58:39 -0400

    set values to test for target in half ranges

--- binary_search.rb
+++ binary_search.rb
@@ -1,13 +1,19 @@
 # given a number and a collection, find the number with least time complexity
 def find_target(target, array)
 
-  # create "bookends" - the start and finish of the search range
-  floor_index = -1
-  ceiling_index = array.length
+    # create "bookends" - the start and finish of the search range
+    floor_index = -1
+    ceiling_index = array.length
 
-  # always need to make sure at least one value between bookends
-  while floor_index+1 < array.length
+    # always need to make sure at least one value between bookends
+    while floor_index+1 < array.length
 
-  end
+        # set values to see if target is in range
+        distance = ceiling_index - floor_index
+        half_distance = distance/2
+        guess_index = floor_index + half_distance
+        guess_value = array[guess_index] 
+
+    end
 
 end
